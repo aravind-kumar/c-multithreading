fd33269d6fd89ba14869eecff4601c4340c83ab1
aravind-kumar <karavindkumar1993@gmail.com>
2017-12-21 23:05:34 -0500

    No crash observerd

--- threading_wit_join.cpp
+++ threading_wit_join.cpp
@@ -7,3 +7,6 @@ int main()
   t.join();
   return 0;
 }
+
+// In the above program the child wouldnt crash because the parent thread would wait for the child to exit
+// Before exiting i.e. the t.join() hence the program wouldnt crash as seen on the case before
